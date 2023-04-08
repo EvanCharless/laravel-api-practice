d3ae89704df8214162b36e60923781579582629b
EvanCharless <evancharles666@gmail.com>
2023-04-09 00:12:14 +0700

    setup auth for retrieve token from laravel sanctum

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,38 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// to retreive token from laravel sanctum manually
+Route::get('/setup', function () {
+    $credentials = [
+        'email' => 'admin@admin.com',
+        'password' => 'password'
+    ];
+
+    if (!Auth::attempt($credentials)) {
+        $user = new \App\Models\User();
+
+        $user->name = 'Admin';
+        $user->email = $credentials['email'];
+        $user->password = Hash::make($credentials['password']);
+
+        $user->save();
+
+        if (Auth::attempt($credentials)) {
+            /** @var \App\Models\User $user **/
+            $user = Auth::user();
+
+            // To assign each role's capabilities
+            $adminToken = $user->createToken('admin-token'); // default = ['*'] / which is means can access all
+            $updateToken = $user->createToken('admin-token', ['create', 'update']);
+            $basicToken = $user->createToken('admin-token', ['read']);
+
+            return [
+                'admin' => $adminToken->plainTextToken,
+                'update' => $updateToken->plainTextToken,
+                'basic' => $basicToken->plainTextToken,
+            ];
+        };
+    };
 });
